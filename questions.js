a8faa2b023bc9e2cae0edc6d11836c8fc6846998
sfcolumbro-oss <s.f.columbro@gmail.com>
2025-09-20 17:29:50 +0200

    in

--- questions.js
+++ questions.js
@@ -1,20 +1,62 @@
 const questions = [
   {
-    question: "Qual è la capitale d’Italia?",
-    answers: ["Milano", "Roma", "Napoli", "Torino"],
+    question: "Per iniziare il Tour dell'Allevamento i Diamantini cerca le stampe appese sulla parete esterna dello stand. Iniziamo?",
+    answers: ["Si, non vedo l'ora di vincere il mio premio", "Non ancora, sto cercando!!"],
+    correct: 0,
+    hint: "Le trovi sulla parete esterna dello stand."
+  },
+  {
+    question: "Che colore alleviamo da i Diamantini?",
+    answers: ["Bianco", "Fulvo", "Nero", "Multicolore"],
+    correct: 1,
+    hint: "Cominicia il tour che trovi sulla parete esterna del nostro stand!"
+  },
+  {
+    question: "Che cos'è il Bosco de i Diamantini oggi?",
+    answers: ["Un prato verde luminoso", "Un bosco fitto di conifere", "Una spiaggia del Lago Maggiore", "Un cortile dove incontrare i barboncini"],
+    correct: 0,
+    hint: "Stai seguendo il Tour sulla parete esterna del nostro stand?"
+  },
+  {
+    question: "Che passato ha il barboncino?",
+    answers: ["Cane da ferma", "Cane da guardia", "Cane da caccia", "Cane da pastore"],
     correct: 2,
-    hint: "È anche chiamata la città eterna."
+    hint: "Hai già 'visitato' il Bosco de i Diamantini?"
   },
   {
-    question: "Quanto fa 5 × 3?",
-    answers: ["8", "15", "10", "20"],
-    correct: 1,
-    hint: "È maggiore di 10."
+    question: "Dove nascono i cuccioli dell'Allevamento i Diamantini?",
+    answers: ["In una nursery calda e accogliente", "In una gabbia rigida e asettica", "In una camera da letto privata", "In un ripostiglio attrezzato"],
+    correct: 0,
+    hint: "Stai seguendo il Tour sulla parete esterna del nostro stand?"
   },
   {
-    question: "Quale animale è noto come 'il migliore amico dell’uomo'?",
-    answers: ["Gatto", "Cane", "Cavallo", "Coniglio"],
+    question: "Che certificazione hanno gli ambienti dell'Allevamento i Diamantini?",
+    answers: ["Non ha certificazioni", "Scuole cinofile unite", "Brevetti canini", "ASL, senza gabbie"],
+    correct: 3,
+    hint: "La risposta è nella Casa"
+  },
+  {
+    question: "Come si chiama lo scioglinodi distracante e setificante de i Diamantini?",
+    answers: ["Crystal Touch", "Velvet Diamond", "Fresh Drops", "Purity Care"],
     correct: 1,
-    hint: "Fa 'bau bau'."
+    hint: "Scopri il nome provandolo all'interno dello stand!"
+  },
+  {
+    question: "Quali libri abbiamo pubblicato?",
+    answers: ["'Tutto sul cucciolo'", "'Tutto sul barboncini'", "Entrambi i precedenti", "Nessuno dei precedenti"],
+    correct: 2,
+    hint: "Scoprili sul tavolo principale dello stand!"
+  },
+  {
+    question: "Come si chiama l'allevatrice de i Diamantini?",
+    answers: ["Francesca", "Giulia", "Rosella", "Ambra"],
+    correct: 2,
+    hint: "Cerca la brochure del nostro allevamento!"
+  },
+  {
+    question: "Fai già parte della nostra community sui social?",
+    answers: ["Certamente!", "Non ancora"],
+  correct: 0,
+    hint: "Seguici alla pagina @i.diamantini su Instagram, TikTok o Facebook e rispondi 'Certamente!!'"
   }
 ];
